1e4020b48942912b5200254008b5a4baca468ea7
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2022-08-28 11:32:17 -0300

    Init project and creation of the OlaMundo component.

---
+++ src/components/OlaMundo.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Olá Mundo</h1>
+  <h2>Primeiro Projeto em Vue3</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "OlaMundo",
+});
+</script>
+
+<style>
+h1 {
+  color: rgb(57, 54, 54);
+}
+</style>
